750f169fb1d582c3450de6df9d0a459cbbd57d5a
Martin Berg Alstad <600878@stud.hvl.no>
2022-03-09 18:02:02 +0100

    Passordfelt

--- F1_Eksempel2_Personer_og_JPA/src/no/hvl/dat107/Passord.java
+++ F1_Eksempel2_Personer_og_JPA/src/no/hvl/dat107/Passord.java
@@ -7,7 +7,16 @@ public class Passord {
     private final String passord;
 
     public Passord() {
-        passord = JOptionPane.showInputDialog("passord:");
+        JPasswordField pf = new JPasswordField();
+        int okCxl = JOptionPane.showConfirmDialog(null, pf,
+                "Passord:", JOptionPane.OK_CANCEL_OPTION);
+
+        if (okCxl == JOptionPane.OK_OPTION) {
+            passord = new String(pf.getPassword());
+        }
+        else {
+            passord = "";
+        }
     }
 
     public String getPassord() {
